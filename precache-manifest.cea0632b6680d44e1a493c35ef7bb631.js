ad8073202cf4f6e9e4bf11eac9078be2e05d6423
Pramod <npramod95@gmail.com>
2019-11-29 17:43:27 +0530

    Updates

---
+++ precache-manifest.cea0632b6680d44e1a493c35ef7bb631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d7460bf35bdef3139dc0b2ee82b553",
+    "url": "/play-youtube-videos/index.html"
+  },
+  {
+    "revision": "bb168b9e978b39f5ede2",
+    "url": "/play-youtube-videos/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "13a91c54b66348ed9531",
+    "url": "/play-youtube-videos/static/css/main.e9774792.chunk.css"
+  },
+  {
+    "revision": "bb168b9e978b39f5ede2",
+    "url": "/play-youtube-videos/static/js/2.37ffa128.chunk.js"
+  },
+  {
+    "revision": "13a91c54b66348ed9531",
+    "url": "/play-youtube-videos/static/js/main.52276ea9.chunk.js"
+  },
+  {
+    "revision": "26eaf971507e3e09b573",
+    "url": "/play-youtube-videos/static/js/runtime-main.bb33623a.js"
+  }
+]);
--- src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import PlaySongsReducer from "./PlaySongsReducer.js";
-
-const rootReducer = combineReducers({
-  songsData: PlaySongsReducer
-});
-
-export default rootReducer;
